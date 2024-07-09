bea8296f36d6f3ec05bbaa01a3bcf428a92a1ea7
Sumanth <141707419+sumanth5885@users.noreply.github.com>
2024-07-09 14:37:27 +0530

    Update StoreContext.jsx

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -5,7 +5,7 @@ export const StoreContext = createContext(null);
 
 const StoreContextProvider = (props) => {
     const [cartItems, setCartItems] = useState({});
-    const url = "http://localhost:4000";
+    const url = "https://hungry-hub-backend-0cji.onrender.com";
     const [token, setTokenState] = useState(
         localStorage.getItem("token") || ""
     );
