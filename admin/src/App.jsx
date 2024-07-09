70810dff2678b4b2519b52a9188b69009ae5664b
Sumanth <141707419+sumanth5885@users.noreply.github.com>
2024-07-09 15:30:06 +0530

    Update App.jsx

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -10,7 +10,7 @@ import 'react-toastify/dist/ReactToastify.css';
 
 const App = () => {
 
-  const url = "http://localhost:4000"
+  const url = "https://hungry-hub-backend-0cji.onrender.com"
   return (
     <div>
       <ToastContainer></ToastContainer>
