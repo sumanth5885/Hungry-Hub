0376a3840d3f2fc41145e0c366f5d0d77643fa86
sumanth5885 <sumanthspoojary58@gmail.com>
2024-09-19 20:36:32 +0530

    loading commit

--- frontend/src/components/FoodDisplay/FoodDisplay.jsx
+++ frontend/src/components/FoodDisplay/FoodDisplay.jsx
@@ -2,11 +2,12 @@ import { useContext } from "react";
 import "./FoodDisplay.css";
 import { StoreContext } from "../../context/StoreContext";
 import FoodItem from "../FoodItem/FoodItem";
+import Hourglass from '../../assets/Hourglass.gif'
 
 const FoodDisplay = ({ category }) => {
   const { food_list, isLoading } = useContext(StoreContext);
 
-  if (isLoading) return <h1>Loading...</h1>
+  if (isLoading) return <img src={Hourglass} alt="" />
 
   return (
     <div className="food-display" id="food-display">
